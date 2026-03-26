6d73783ad9a9a66a44675870a4231e574e3d9bce
Kakici <84840724+kakici28@users.noreply.github.com>
2026-03-26 23:49:40 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,33 +1,38 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
 const cors = require("cors");
 const axios = require("axios");
 
 const app = express();
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(cors());
 
 const logevents = [];
+const users = [];
 
-app.get("/_endpoint_", (req, res) => {
-  
+app.post("/register", (req, res) => {
+  const { username } = req.body;
+  const token = randomBytes(4).toString("hex");
+  users.push({ username, token });
+  logevents.push({ action: "register", username });
+  res.status(201).send({ username, token });
 });
 
-app.post("/_endpoint_",  (req, res) => {
-
-  res.status(201);
+app.post("/login", (req, res) => {
+  const { username } = req.body;
+  const user = users.find(u => u.username === username);
+  logevents.push({ action: "login", username });
+  if (user) res.send(user);
+  else res.status(404).send("User not found");
 });
 
-
-app.post("/events", async (req, res) => {
-  
+app.post("/events", (req, res) => {
+    logs.push(req.body);
   res.send({});
 });
+
 app.get("/logevents", (req, res) => {
   res.send(logevents);
 });
 
-app.listen(4000, () => {
-  console.log("Listening on 4000 to _serviceName_");
-});
+app.listen(6000, () => console.log("Listening on 6000 to auth-service"));
